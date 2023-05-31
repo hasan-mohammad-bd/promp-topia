73a44c4bd4622f1cfa15c3561155592b5a3220d0
hasan-mohammad-bd <hasan.md.east@gmail.com>
2023-05-31 18:07:17 +0600

    Nav created

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import image_1 from "../public/assets/images/logo.svg";
 
 const Nav = () => {
-    return (
-        <div>
-            Nav
-            
-        </div>
-    );
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src={image_1}
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p>Promptopia</p>
+      </Link>
+
+      {/* Mobile navigation */}
+      <div className="sm:flex hidden">
+        
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
---
+++ components/PromptCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PromptCard = () => {
+    return (
+        <div>
+            This is promptCard
+        </div>
+    );
+};
+
+export default PromptCard;
